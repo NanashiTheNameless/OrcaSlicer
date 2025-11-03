7bb8f8092e52b01aa12db677a2db62f5a0b43947
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-11-03 09:42:08 -0600

    Fix httpbin.org 503 by switching to maintained httpbingo

--- tests/slic3rutils/slic3rutils_tests_main.cpp
+++ tests/slic3rutils/slic3rutils_tests_main.cpp
@@ -3,25 +3,25 @@
 #include "slic3r/Utils/Http.hpp"
 
 TEST_CASE("Check SSL certificates paths", "[Http][NotWorking]") {
-    
+
     Slic3r::Http g = Slic3r::Http::get("https://github.com/");
-    
+
     unsigned status = 0;
     g.on_error([&status](std::string, std::string, unsigned http_status) {
         status = http_status;
     });
-    
+
     g.on_complete([&status](std::string /* body */, unsigned http_status){
         status = http_status;
     });
-    
+
     g.perform_sync();
-    
+
     REQUIRE(status == 200);
 }
 
 TEST_CASE("Http digest authentication", "[Http][NotWorking]") {
-    Slic3r::Http g = Slic3r::Http::get("https://httpbin.org/digest-auth/auth/guest/guest");
+    Slic3r::Http g = Slic3r::Http::get("https://httpbingo.org/digest-auth/auth/guest/guest");
 
     g.auth_digest("guest", "guest");
 
@@ -40,7 +40,7 @@ TEST_CASE("Http digest authentication", "[Http][NotWorking]") {
 }
 
 TEST_CASE("Http basic authentication", "[Http][NotWorking]") {
-    Slic3r::Http g = Slic3r::Http::get("https://httpbin.org/basic-auth/guest/guest");
+    Slic3r::Http g = Slic3r::Http::get("https://httpbingo.org/basic-auth/guest/guest");
 
     g.auth_basic("guest", "guest");
 
